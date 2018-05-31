4228ff013f818a8e9ecb358df998c05ffd7d01f1
fgorina <fgorina@gmail.com>
2018-05-31 08:48:13 -0700

    First version

---
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -0,0 +1,84 @@
+from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import rospy
+
+class TLClassifier(object):
+    def __init__(self):
+        #TODO load classifier
+
+        self.detection_graph = self.load_graph("/home/student/CarND-Capstone/ros/ssd_bags_20K/frozen_inference_graph.pb")
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.sess = tf.Session(graph=self.detection_graph)
+
+    def filter_boxes(self, min_score, boxes, scores, classes):
+        n = len(classes)
+        idxs = []
+        for i in range(n):
+            if scores[i] >= min_score:
+                idxs.append(i)
+
+        filtered_boxes = boxes[idxs, ...]
+        filtered_scores = scores[idxs, ...]
+        filtered_classes = classes[idxs, ...]
+
+        return filtered_boxes, filtered_scores, filtered_classes
+
+
+    def load_graph(self, graph_file):
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        return graph
+
+
+
+    def get_classification(self, image):
+        """Determines the color of the traffic light in the image
+
+        Args:
+            image (cv::Mat): image containing the traffic lightA
+
+        
+
+        Returns:
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
+        #TODO implement light color prediction
+
+        #rospy.logwarn("Received Image")
+        image_np = np.expand_dims(np.asarray(image, dtype=np.uint8)[:, :, 0:3], 0)
+        (boxes, scores, classes) = self.sess.run([self.detection_boxes, self.detection_scores, self.detection_classes],
+            feed_dict={self.image_tensor: image_np})
+
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes)
+
+        #rospy.logwarn("Max Score " + str(max(scores)))
+
+        confidence_cutoff = 0.6
+        (boxes, scores, classes) = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
+
+        if len(classes) > 0:
+            color = min(classes)
+            # rospy.logwarn("Image color " + str(classes)+ "  " + str(color))
+
+	    if color == 3:
+                return TrafficLight.GREEN
+            elif color == 2:
+                return TrafficLight.YELLOW
+            else:
+                return TrafficLight.RED
+
+        return TrafficLight.UNKNOWN
+
+
---
+++ ros/src/twist_controller/twist_controller.py
@@ -0,0 +1,73 @@
+import rospy
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
+
+GAS_DENSITY = 2.858
+ONE_MPH = 0.44704
+
+
+class Controller(object):
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit,
+                    wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
+        # TODO: Implement
+
+        self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+
+        # PID Constants - Experimental values
+        kp = 0.3
+        ki = 0.1
+        kd = 0.0
+
+        # Minimum and maximum throttle values
+        mn = 0.0
+        mx = 0.2
+
+        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+
+        tau = 0.5  # For the LP filter. 1/(2pi*tau) = cutoff frequency
+        ts = 0.2   # 1/50 = sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+
+	self.vehicle_mass = vehicle_mass   # Used to compute torque
+        self.fuel_capacity = fuel_capacity # If we want to be perfect with the mass
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+
+        self.last_time = rospy.get_time()
+
+
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+        # TODO: Change the arg, kwarg list to suit your needs
+
+        if not dbw_enabled:  # Manual Drive. Reset PID son I term doesn't integrate
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+
+        current_vel = self.vel_lpf.filt(current_vel)    # Remove too fast transients in speed
+
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
+        vel_error = linear_vel - current_vel
+        self.last_vel = current_vel
+
+        # rospy.logwarn("Steer : " + str(steering) + "Speed: " + str(linear_vel)+ " Real Speed:" + str(current_vel))
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0
+
+        if linear_vel == 0. and current_vel < 0.1:
+            throttle = 0
+            brake = 400        # N*m. Stop and fix the car in place braking hard
+
+        elif throttle < .2 and vel_error < 0:    # too fast, m ust brake
+            throttle = 0
+            decel = max(vel_error , self.decel_limit)
+            brake = abs(decel) * self.vehicle_mass*self.wheel_radius
+
+        return throttle, brake, steering
