e7c07eef0d5ead15c88c72ae70af40cda6ad83d7
Luciano Silveira <ladrians@gmail.com>
2018-07-02 03:25:00 -0700

    new process_csv script to plot csv files generated from dbw_test.py

---
+++ ros/src/twist_controller/process_csv.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Ideas from https://pythonprogramming.net/loading-file-data-matplotlib-tutorial/
+import matplotlib.pyplot as plt
+import csv
+
+def plot_file(file_name, title, legend1, legend2):
+    '''
+    generates a graph with plotting data
+    '''
+    x = []
+    y = []
+    z = []
+    with open(file_name,'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        count = 0
+        for row in plots:
+            count += 1
+            if count == 1:
+                continue
+            x.append(count)
+            y.append(float(row[0]))
+            z.append(float(row[1]))
+
+    plt.gca().set_color_cycle(['red', 'blue', 'green', 'yellow'])
+
+    plt.plot(x,y, label=legend1)
+    plt.plot(x,z, label=legend2)
+
+    plt.title(title)
+    plt.legend([legend1, legend2], loc='upper left')
+    plt.savefig('{}.png'.format(title))
+
+# Process CSV files
+plot_file('throttles.csv', 'throttles', 'actual', 'proposed')
+plot_file('steers.csv', 'steers', 'actual', 'proposed')
+plot_file('brakes.csv', 'brakes', 'actual', 'proposed')
+
+
