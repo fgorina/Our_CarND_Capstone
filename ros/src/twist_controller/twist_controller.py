a54a9d8a4c8d71d2f9c104f1966e88ffe7dc79c0
Luciano Silveira <ladrians@gmail.com>
2018-07-02 03:21:17 -0700

    improvements in twist controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -17,7 +17,7 @@ class Controller(object):
         # PID Constants - Experimental values
         kp = 0.3
         ki = 0.1
-        kd = 0.0
+        kd = 0.005
 
         # Minimum and maximum throttle values
         mn = 0.0
@@ -26,7 +26,7 @@ class Controller(object):
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
         tau = 0.5  # For the LP filter. 1/(2pi*tau) = cutoff frequency
-        ts = 0.2   # 1/50 = sample time
+        ts = 0.02   # 1/rate = sample time
         self.vel_lpf = LowPassFilter(tau, ts)
 
 	self.vehicle_mass = vehicle_mass   # Used to compute torque
@@ -53,7 +53,7 @@ class Controller(object):
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
 
-        # rospy.logwarn("Steer : " + str(steering) + "Speed: " + str(linear_vel)+ " Real Speed:" + str(current_vel))
+        #rospy.logwarn("Steer:%.2f"%steering + " Speed:%.2f"%linear_vel + " Real Speed:%.2f"%current_vel)
         current_time = rospy.get_time()
         sample_time = current_time - self.last_time
         self.last_time = current_time
@@ -63,11 +63,11 @@ class Controller(object):
 
         if linear_vel == 0. and current_vel < 0.1:
             throttle = 0
-            brake = 400        # N*m. Stop and fix the car in place braking hard
+            brake = 400 # N*m. Stop and fix the car in place braking hard
 
-        elif throttle < .2 and vel_error < 0:    # too fast, m ust brake
+        elif throttle < .2 and vel_error < 0: # too fast, must brake
             throttle = 0
             decel = max(vel_error , self.decel_limit)
-            brake = abs(decel) * self.vehicle_mass*self.wheel_radius
-
+            brake = abs(decel) * (self.vehicle_mass+self.fuel_capacity*GAS_DENSITY)*self.wheel_radius # Torque N*s
+ 
         return throttle, brake, steering
