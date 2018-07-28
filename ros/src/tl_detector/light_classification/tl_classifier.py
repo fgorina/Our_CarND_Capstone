dc325b9a6d6360f08ad440d9efe5237e2b686964
Luciano Silveira <ladrians@gmail.com>
2018-07-27 19:54:58 -0700

    adding /image_debug topic to check light classifier behavior

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,6 +2,11 @@ from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import numpy as np
 import rospy
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+
+debug = True # Change here to disable Image debugging
 
 class TLClassifier(object):
     def __init__(self):
@@ -13,6 +18,10 @@ class TLClassifier(object):
         self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
         self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
         self.sess = tf.Session(graph=self.detection_graph)
+
+        if debug == True:
+            self.image_debug_pub = rospy.Publisher('/image_debug', Image, queue_size=1)
+            self.bridge = CvBridge()
 
     def get_graph_path(self):
         tf_version = tf.__version__
@@ -60,7 +69,6 @@ class TLClassifier(object):
         """
         #TODO implement light color prediction
 
-        #rospy.logwarn("Received Image")
         image_np = np.expand_dims(np.asarray(image, dtype=np.uint8)[:, :, 0:3], 0)
         (boxes, scores, classes) = self.sess.run([self.detection_boxes, self.detection_scores, self.detection_classes],
             feed_dict={self.image_tensor: image_np})
@@ -74,17 +82,48 @@ class TLClassifier(object):
         confidence_cutoff = 0.6
         (boxes, scores, classes) = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
 
+        detected_value = TrafficLight.UNKNOWN
+
         if len(classes) > 0:
             color = min(classes)
-            # rospy.logwarn("Image color " + str(classes)+ "  " + str(color))
+            #rospy.logwarn("Image color " + str(classes)+ "  " + str(color))
 
 	    if color == 3:
-                return TrafficLight.GREEN
+                detected_value = TrafficLight.GREEN
             elif color == 2:
-                return TrafficLight.YELLOW
+                detected_value = TrafficLight.YELLOW
             else:
-                return TrafficLight.RED
+                detected_value = TrafficLight.RED
 
-        return TrafficLight.UNKNOWN
+        if debug == True:
+            self.show_detection(image, boxes, detected_value)
+        return detected_value
 
+    def show_detection(self, image, boxes, detected_value):
+        """Display the detection as a new topic with the associated color"""
+
+        img_debug = np.copy(image)
+        for i, box in enumerate(boxes):
+            x1 = int(box[0] * image.shape[0])
+            y1 = int(box[1] * image.shape[1])
+            x2 = int(box[2] * image.shape[0])
+            y2 = int(box[3] * image.shape[1])
+
+            MIX_IMAGE_HEIGHT = 50 # too small detection, discard it
+            if image.shape[0] < MIX_IMAGE_HEIGHT:
+                continue
+
+            color = [255,255,255]
+            if detected_value == TrafficLight.RED:
+                color = [0,0,255]
+            elif detected_value == TrafficLight.YELLOW:
+                color = [0,255,255]
+            elif detected_value == TrafficLight.GREEN:
+                color = [0,255,0]
+            cv2.rectangle(img_debug, (y1, x1), (y2, x2), color, thickness=-1)
+        alpha = 0.4
+        cv2.addWeighted(img_debug, alpha, image, 1 - alpha, 0, img_debug)
+
+        new_image = self.bridge.cv2_to_imgmsg(img_debug, encoding='bgr8')
+        self.image_debug_pub.publish(new_image)
 
